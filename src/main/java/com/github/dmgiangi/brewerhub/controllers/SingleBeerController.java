f4429fc14b2fa6532e1d0eb1bcb87ab66036ce8b
Gianluigi De Marco <dmgiangi@gmail.com>
2022-07-11 17:36:44 +0200

    add in SingleBeerController Random beer functionality

--- src/main/java/com/github/dmgiangi/brewerhub/controllers/SingleBeerController.java
+++ src/main/java/com/github/dmgiangi/brewerhub/controllers/SingleBeerController.java
@@ -1,18 +1,28 @@
-package com.github.dmgiangi.brewerhub.contrrollers;
+package com.github.dmgiangi.brewerhub.controllers;
 
 import com.github.dmgiangi.brewerhub.dao.BeersDAO;
 import com.github.dmgiangi.brewerhub.models.Beer;
 import com.github.dmgiangi.brewerhub.utilities.SqlConnectionFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/beers")
 public class SingleBeerController {
-   @GetMapping("/beers/{id}")
+   @GetMapping("/{id}")
    public Beer getBeer(@PathVariable int id){
       SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
       Beer beer = new BeersDAO(connectionFactory.getConnection()).selectBeerById(id);
+      connectionFactory.disconnect();
+      return beer;
+   }
+
+   @GetMapping("/random")
+   public Beer getRandomBeer(){
+      SqlConnectionFactory connectionFactory = new SqlConnectionFactory();
+      Beer beer = new BeersDAO(connectionFactory.getConnection()).selectRandomBeer();
       connectionFactory.disconnect();
       return beer;
    }
