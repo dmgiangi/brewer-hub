9174d07cf1654eb770710a3d05723b11ff554352
Gianluigi De Marco <dmgiangi@gmail.com>
2022-07-11 21:46:08 +0200

    removed unused imports

--- src/main/java/com/github/dmgiangi/brewerhub/BrewerHubApplication.java
+++ src/main/java/com/github/dmgiangi/brewerhub/BrewerHubApplication.java
@@ -1,20 +1,9 @@
 package com.github.dmgiangi.brewerhub;
 
-import com.github.dmgiangi.brewerhub.dao.BeersDAO;
-import com.github.dmgiangi.brewerhub.exceptions.InsertException;
-import com.github.dmgiangi.brewerhub.models.Beer;
-import com.github.dmgiangi.brewerhub.models.BeersList;
-import com.github.dmgiangi.brewerhub.utilities.SqlConnectionFactory;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class BrewerHubApplication {
