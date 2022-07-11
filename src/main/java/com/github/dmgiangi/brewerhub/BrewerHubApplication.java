33527175e90272117f00a288c1c89d0e41023810
Gianluigi De Marco <dmgiangi@gmail.com>
2022-07-11 11:58:34 +0200

    add main in BrewerHubApplication

--- src/main/java/com/github/dmgiangi/brewerhub/BrewerHubApplication.java
+++ src/main/java/com/github/dmgiangi/brewerhub/BrewerHubApplication.java
@@ -1,9 +1,9 @@
 package com.github.dmgiangi.brewerhub;
 
 import com.github.dmgiangi.brewerhub.dao.BeersDAO;
+import com.github.dmgiangi.brewerhub.exceptions.InsertException;
 import com.github.dmgiangi.brewerhub.models.Beer;
 import com.github.dmgiangi.brewerhub.models.BeersList;
-import com.github.dmgiangi.brewerhub.utilities.DatabasePropertyLoader;
 import com.github.dmgiangi.brewerhub.utilities.SqlConnectionFactory;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class BrewerHubApplication {
@@ -21,29 +22,5 @@ public class BrewerHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrewerHubApplication.class, args);
-	}
-
-	/*public static void main(String[] args) {
-        Gson gson = new Gson();
-        Reader reader = new BrewerHubApplication().getJsonReaderFromResources("/beers.json");
-        BeersList beersList = gson.fromJson(reader, new TypeToken<BeersList>(){}.getType());
-		System.out.println(beersList);
-		BeersDAO beersDAO = new BeersDAO(new SqlConnectionFactory().getConnection());
-		for (Beer beer: beersList) {
-			logger.info("START OF INSERTING OF BEER WITH ID: " + beer.getId());
-			beersDAO.insertBeer(beer);
-			logger.info("END OF INSERTING OF BEER WITH ID: " + beer.getId());
-		}
-	}*/
-
-
-	private Reader getJsonReaderFromResources(String filePath) {
-		Reader reader = null;
-		try {
-			reader = new InputStreamReader(this.getClass().getResourceAsStream(filePath));
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
-		return reader;
 	}
 }
