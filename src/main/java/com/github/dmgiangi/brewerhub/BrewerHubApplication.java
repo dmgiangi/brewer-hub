55e64cd10f67a80875e354cc9c9a80d298cd518f
Gianluigi De Marco <dmgiangi@gmail.com>
2022-07-12 17:58:01 +0200

    add maven dependencies spring-boot-starter-actuator

--- src/main/java/com/github/dmgiangi/brewerhub/BrewerHubApplication.java
+++ src/main/java/com/github/dmgiangi/brewerhub/BrewerHubApplication.java
@@ -4,9 +4,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BrewerHubApplication {
+public class BrewerHubApplication extends SpringBootServletInitializer {
 	final static Logger logger = LogManager.getLogger(BrewerHubApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BrewerHubApplication.class, args);
