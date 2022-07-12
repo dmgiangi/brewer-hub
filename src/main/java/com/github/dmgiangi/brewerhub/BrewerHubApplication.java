233e91050a87099268c9c03cb2ed6630e5ceb307
Gianluigi De Marco <dmgiangi@gmail.com>
2022-07-12 19:17:55 +0200

    add maven dependencies spring-boot-starter-actuator

--- src/main/java/com/github/dmgiangi/brewerhub/BrewerHubApplication.java
+++ src/main/java/com/github/dmgiangi/brewerhub/BrewerHubApplication.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -11,5 +12,10 @@ public class BrewerHubApplication extends SpringBootServletInitializer {
 	final static Logger logger = LogManager.getLogger(BrewerHubApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BrewerHubApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BrewerHubApplication.class);
 	}
 }
--- src/main/java/com/github/dmgiangi/brewerhub/controllers/BeersController.java
+++ src/main/java/com/github/dmgiangi/brewerhub/controllers/BeersController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.Date;
 
 @RestController
 public class BeersController {
