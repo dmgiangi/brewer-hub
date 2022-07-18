69657d26fd09d092b41d26bf0bbe28f11f2c5e54
Gianluigi De Marco <dmgiangi@gmail.com>
2022-07-18 16:40:09 +0200

    add filter for ip rate limit

---
+++ src/main/java/com/github/dmgiangi/brewerhub/services/IpFilter.java
@@ -0,0 +1,68 @@
+package com.github.dmgiangi.brewerhub.services;
+
+import com.github.dmgiangi.brewerhub.models.response.Error;
+import com.google.gson.Gson;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component
+public class IpFilter implements Filter {
+        final static Logger logger = LogManager.getLogger(IpFilter.class);
+
+        private static final Bandwidth limit = Bandwidth.simple(60, Duration.ofMinutes(1));
+
+        private Map<String, Bucket> buckets;
+
+        @Override
+        public void init(FilterConfig filterConfig) {
+                buckets = new HashMap<>();
+        }
+
+        @Override
+        public void destroy() { Filter.super.destroy(); }
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+                String ip = request.getRemoteAddr();
+
+                buckets.putIfAbsent(ip,
+                        Bucket.builder()
+                                .addLimit(limit)
+                                .build());
+                Bucket bucket = buckets.get(ip);
+
+                HttpServletResponse httpResponse = (HttpServletResponse) response;
+                httpResponse.setHeader("x-ratelimit-limit:", "60");
+
+                if (bucket.tryConsume(1)) {
+                        httpResponse.setHeader(
+                                "x-ratelimit-limit-remaining:",
+                                bucket.getAvailableTokens() + "");
+                        chain.doFilter(request, httpResponse);
+                } else {
+                        // limit is exceeded
+
+                        httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                        httpResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                        httpResponse.setHeader("x-ratelimit-limit-remaining:", "0");
+                        httpResponse.getWriter().write(
+                                new Gson().toJson(
+                                        new Error("Too many request" , "429")));
+                        httpResponse.getWriter().flush();
+                }
+        }
+}
